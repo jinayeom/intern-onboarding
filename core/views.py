dad91bd389a35fef80b0aed6f7f27e587eafba0d
jinayeom <jinayeomcs@gmail.com>
2025-10-14 13:43:38 -0700

    Added Modules page and few topics

--- core/views.py
+++ core/views.py
@@ -11,3 +11,13 @@ def home(request):
         "roadmaps": roadmaps, 
     }
     return render(request, "core/home.html", context)
+
+def modules_index(request): 
+    topics = [
+        {"title": "Setting up your Development Environment", "slug":"ssh-dev-env"},
+        {"title": "Creating a Branch"},
+        {"title": "How to Compile your Code"},
+    ]
+    topics = sorted(topics, key=lambda t: t["title"].lower())
+    return render(request, "core/modules/index.html", {"topics": topics})
+

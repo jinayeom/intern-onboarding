bbc9d54895e90a62386ac7fdc54f605277a71301
jinayeom <jinayeomcs@gmail.com>
2025-10-14 14:18:37 -0700

    Add pages topics in Modules page

--- core/urls.py
+++ core/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'), 
     path("modules/", views.modules_index, name="modules"), 
+    path("modules/<slug:slug>/", views.module_detail, name="module-detail"),
 ]
--- core/views.py
+++ core/views.py
@@ -1,6 +1,12 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 from django.shortcuts import render
 from roadmap.models import Roadmap  
+
+TOPICS = [
+        {"title": "Setting up your Development Environment", "slug":"ssh-dev-env"},
+        {"title": "Creating a Branch", "slug":"creating-branch"},
+        {"title": "How to Compile your Code", "slug":"compile-code"},
+    ]
 
 def home(request): 
     roadmaps = Roadmap.objects.order_by("title")  
@@ -12,12 +18,13 @@ def home(request):
     }
     return render(request, "core/home.html", context)
 
-def modules_index(request): 
-    topics = [
-        {"title": "Setting up your Development Environment", "slug":"ssh-dev-env"},
-        {"title": "Creating a Branch"},
-        {"title": "How to Compile your Code"},
-    ]
-    topics = sorted(topics, key=lambda t: t["title"].lower())
-    return render(request, "core/modules/index.html", {"topics": topics})
+def modules_index(request):
+    topics = sorted(TOPICS, key=lambda t: t["title"].lower())
+    return render(request, "core/modules/index.html", {"topics": TOPICS})
 
+def module_detail(request, slug): 
+    topics = TOPICS 
+    module = next((t for t in topics if t.get("slug") == slug), None) 
+    if not module: 
+        raise Http404("Module not found") 
+    return render(request, "core/modules/detail.html", {"module": module})
